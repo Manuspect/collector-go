89217daafa47981548b28ab17b39bfe5c0c73ca5
Misha-M33 <mihnikm@gmail.com>
2024-07-10 14:52:37 +0300

    feat: add swagger to UloadFile

--- controllers/s3_controller.go
+++ controllers/s3_controller.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"context"
 	"fmt"
-	"log"
+
+	logFi "github.com/gofiber/fiber/v2/log"
+
 	"os"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,12 +14,23 @@ import (
 	"github.com/oklog/ulid/v2"
 )
 
+// @Summary		UploadFile return file with bucket name
+// @Description	UploadFile retrieves the value of the environment variable named by the key and return fileUpload
+// @Tags		Upload
+// @Accept		json
+// @Produce		json
+// @Param		fileUpload		formData	file					true	"path to upload formData file"
+// @Success		200				{object}	entities.InfoFile
+// @Failure		400				{object}	entities.ServerError
+// @Failure		500				{object}	entities.ServerError
+// @Router		/api/v1/upload	[post]
 func UploadFile(m *minio.Client, js jetstream.JetStream) func(c *fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 		ctx := context.Background()
 		bucketName := os.Getenv("MINIO_BUCKET")
 		file, err := c.FormFile("fileUpload")
 		if err != nil {
+			logFi.Error("UploadFile")
 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 				"error": true,
 				"msg":   err.Error(),
@@ -26,6 +39,7 @@ func UploadFile(m *minio.Client, js jetstream.JetStream) func(c *fiber.Ctx) erro
 
 		buffer, err := file.Open()
 		if err != nil {
+			logFi.Error("UploadFile")
 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 				"error": true,
 				"msg":   err.Error(),
@@ -47,6 +61,7 @@ func UploadFile(m *minio.Client, js jetstream.JetStream) func(c *fiber.Ctx) erro
 			minio.PutObjectOptions{ContentType: contentType},
 		)
 		if err != nil {
+			logFi.Error("UploadFile")
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 				"error": true,
 				"msg":   err.Error(),
@@ -62,9 +77,9 @@ func UploadFile(m *minio.Client, js jetstream.JetStream) func(c *fiber.Ctx) erro
 				bucketName)),
 		)
 
-		log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+		logFi.Infof("Successfully uploaded %s of size %d\n", objectName, info.Size)
 
-		return c.JSON(fiber.Map{
+		return c.Status(200).JSON(fiber.Map{
 			"error": false,
 			"msg":   nil,
 			"info":  info,
